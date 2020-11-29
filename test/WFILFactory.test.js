ff44b834a18955df7e138f4a4ccb71542675e29b
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-11-29 01:03:04 +0000

    add setCustodianDeposit tests + update tests

--- test/WFILFactory.test.js
+++ test/WFILFactory.test.js
@@ -16,7 +16,7 @@ let wfil;
 let factory;
 
 describe('WFIL', function () {
-const [ deployer, dao, owner, newOwner, merchant, custodian, other ] = accounts;
+const [ deployer, dao, owner, newOwner, merchant, merchant2, custodian, custodian2, other ] = accounts;
 
 
 const amount = ether('10');
@@ -28,9 +28,13 @@ const PAUSER_ROLE = web3.utils.soliditySha3('PAUSER_ROLE');
 
 const ZERO = 0;
 
+const deposit = 't3q32q2hmq63tpgejlsuubkqjpfqhv75vu2ieg2jhyqhob7dikuftf4mjhobueuurnb77v67rnhr7diz6l2iaq';
+
   beforeEach(async function () {
     wfil = await WFIL.new(dao, { from: deployer });
     factory = await WFILFactory.new(wfil.address, owner, { from: deployer });
+    await factory.addCustodian(custodian, {from: owner});
+    await factory.addMerchant(merchant, {from:owner});
   });
 
   describe('Setup', async function () {
@@ -70,90 +74,102 @@ const ZERO = 0;
 
     it("should revert when account is set to zero address", async () => {
       await expectRevert(factory.setOwner(ZERO_ADDRESS, {from:owner}), 'WFILFactory: new owner is the zero address');
-    })
+    });
 
     it('other accounts cannot set a new owner', async function () {
       await expectRevert(factory.setOwner(newOwner, { from: other }),'WFILFactory: caller is not the default admin');
     });
   });
 
+  describe('setCustodianDeposit()', function () {
+    it('custodian can set custodian deposit address', async function () {
+      const receipt = await factory.setCustodianDeposit(merchant, deposit, { from: custodian });
+      expect(await factory.custodian(merchant)).to.equal(deposit);
+    });
+
+    it('should emit the appropriate event when new custodian address is set', async () => {
+      const receipt = await factory.setCustodianDeposit(merchant, deposit, {from: custodian});
+      expectEvent(receipt, 'CustodianDepositSet', { merchant: merchant, custodian: custodian, deposit: deposit });
+    });
+
+    it("should revert when merchant is set to zero address", async () => {
+      await expectRevert(factory.setCustodianDeposit(ZERO_ADDRESS, deposit, {from:custodian}), 'WFILFactory: invalid merchant address');
+    });
+
+    it('other accounts cannot set a new custodian deposit address', async function () {
+      await expectRevert(factory.setCustodianDeposit(merchant, deposit, { from: other }),'WFILFactory: caller is not a custodian');
+    });
+  });
+
   describe("addCustodian()", async () => {
       it("default admin should be able to add a new custodian", async () => {
-        await factory.addCustodian(custodian, {from:owner});
-        expect(await factory.getRoleMember(CUSTODIAN_ROLE, 0)).to.equal(custodian);
-      })
+        await factory.addCustodian(custodian2, {from:owner});
+        expect(await factory.getRoleMember(CUSTODIAN_ROLE, 1)).to.equal(custodian2);
+      });
 
       it("should emit the appropriate event when a new custodian is added", async () => {
-        const receipt = await factory.addCustodian(custodian, {from:owner});
-        expectEvent(receipt, "RoleGranted", { account: custodian });
-      })
+        const receipt = await factory.addCustodian(custodian2, {from:owner});
+        expectEvent(receipt, "RoleGranted", { account: custodian2 });
+      });
 
       it("should revert when account is set to zero address", async () => {
         await expectRevert(factory.addCustodian(ZERO_ADDRESS, {from:owner}), 'WFILFactory: account is the zero address');
-      })
+      });
 
       it("other address should not be able to add a new custodian", async () => {
-        await expectRevert(factory.addCustodian(custodian, {from:other}), 'WFILFactory: caller is not the default admin');
-      })
-  })
+        await expectRevert(factory.addCustodian(custodian2, {from:other}), 'WFILFactory: caller is not the default admin');
+      });
+  });
 
   describe("removeCustodian()", async () => {
-      beforeEach(async () => {
-        await factory.addCustodian(custodian, {from: owner});
-      })
-
       it("default admin should be able to remove a custodian", async () => {
         await factory.removeCustodian(custodian, {from:owner});
         expect(await factory.hasRole(CUSTODIAN_ROLE, custodian)).to.equal(false);
-      })
+      });
 
       it("should emit the appropriate event when a custodian is removed", async () => {
         const receipt = await factory.removeCustodian(custodian, {from:owner});
         expectEvent(receipt, "RoleRevoked", { account: custodian });
-      })
+      });
 
       it("other address should not be able to remove a custodian", async () => {
         await expectRevert(factory.removeCustodian(custodian, {from: other}), 'WFILFactory: caller is not the default admin');
-      })
-  })
+      });
+  });
 
   describe("addMerchant()", async () => {
       it("default admin should be able to add a new merchant", async () => {
-        await factory.addMerchant(merchant, {from:owner});
-        expect(await factory.getRoleMember(MERCHANT_ROLE, 0)).to.equal(merchant);
-      })
+        await factory.addMerchant(merchant2, {from:owner});
+        expect(await factory.getRoleMember(MERCHANT_ROLE, 1)).to.equal(merchant2);
+      });
 
       it("should emit the appropriate event when a new merchant is added", async () => {
-        const receipt = await factory.addMerchant(merchant, {from:owner});
-        expectEvent(receipt, "RoleGranted", { account: merchant });
-      })
+        const receipt = await factory.addMerchant(merchant2, {from:owner});
+        expectEvent(receipt, "RoleGranted", { account: merchant2 });
+      });
 
       it("should revert when account is set to zero address", async () => {
         await expectRevert(factory.addMerchant(ZERO_ADDRESS, {from:owner}), 'WFILFactory: account is the zero address');
-      })
+      });
 
       it("other address should not be able to add a new merchant", async () => {
-        await expectRevert(factory.addMerchant(merchant, {from:other}), 'WFILFactory: caller is not the default admin');
-      })
-  })
+        await expectRevert(factory.addMerchant(merchant2, {from:other}), 'WFILFactory: caller is not the default admin');
+      });
+  });
 
   describe("removeMerchant()", async () => {
-      beforeEach(async () => {
-        await factory.addMerchant(merchant, {from: owner});
-      })
-
       it("default admin should be able to remove a merchant", async () => {
         await factory.removeMerchant(merchant, {from:owner});
         expect(await factory.hasRole(MERCHANT_ROLE, merchant)).to.equal(false);
-      })
+      });
 
       it("should emit the appropriate event when a merchant is removed", async () => {
         const receipt = await factory.removeMerchant(merchant, {from:owner});
         expectEvent(receipt, "RoleRevoked", { account: merchant });
-      })
+      });
 
       it("other address should not be able to remove a merchant", async () => {
         await expectRevert(factory.removeMerchant(merchant, {from: other}), 'WFILFactory: caller is not the default admin');
-      })
-  })
+      });
+  });
 });
