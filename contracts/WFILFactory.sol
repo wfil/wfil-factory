d6e95991fba24765be7d70b1c46a8c7bd4641bf8
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2020-12-20 22:52:31 +0000

    update wfil factory ascii art

--- contracts/WFILFactory.sol
+++ contracts/WFILFactory.sol
@@ -11,7 +11,7 @@ pragma solidity 0.6.12;
 /*
 ╦ ╦╔═╗╦╦    ╔═╗┌─┐┌─┐┌┬┐┌─┐┬─┐┬ ┬
 ║║║╠╣ ║║    ╠╣ ├─┤│   │ │ │├┬┘└┬┘
-╚╩╝╚  ╩╩═╝  ╚  ┴ ┴└─┘ ┴ └─┘┴└─ ┴ 
+╚╩╝╚  ╩╩═╝  ╚  ┴ ┴└─┘ ┴ └─┘┴└─ ┴
 */
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
